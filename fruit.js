115a314c95fcc702c90f00ec9c3003e8c09a2f17
John Doe <nadia643@gmail.com>
2020-07-01 11:47:55 +0100

    centered canvas

--- fruit.js
+++ fruit.js
@@ -2,6 +2,20 @@ function Fruit() {
   this.x;
   this.y;
 
+  // the function makes the square flash diff colours
+  // function getRandomColour(){
+  //   let red = Math.floor(Math.random()* 255);
+  //   let green = Math.floor(Math.random() * 255);
+  //   let blue = Math.floor(Math.random() * 255);
+  
+  //   return "rgb("+red+","+green+"," +blue+" )";  
+  // }
+  // this makes it a diff colour on every refresh of the page
+  let red = Math.floor(Math.random()* 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let getRandomColour = "rgb("+red+","+green+"," +blue+" )";
+
   this.choseInitialPlace = function() {
     this.x = (Math.floor(Math.random() * rows - 1) + 1) * scale;
     this.y = (Math.floor(Math.random() * columns - 1) + 1) * scale;
@@ -9,7 +23,7 @@ function Fruit() {
   }
 
   this.draw = function() {
-    ctx.fillStyle = "#4cafab";
+    ctx.fillStyle = getRandomColour;
     ctx.fillRect(this.x, this.y, scale, scale)
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
+
+
+ctx.stroke();
+
 const scale = 40;
 
 const rows = canvas.height / scale;
@@ -16,6 +20,7 @@ let fruit;
   window.setInterval(() => {
     // moves the rect along instead of adding more
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     fruit.draw();
     snake.update();
     snake.draw();
