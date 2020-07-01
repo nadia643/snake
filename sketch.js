dbf06dccf365627754b6422c0f32d719e65c6748
John Doe <nadia643@gmail.com>
2020-07-01 11:13:12 +0100

    snake goes  back to zero when caught

--- sketch.js
+++ sketch.js
@@ -5,23 +5,35 @@ const scale = 40;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
+let snake;
+let fruit; 
+
 (function setup() {
   snake = new Snake();
-  
+  fruit = new Fruit();  
+  fruit.choseInitialPlace();
 
   window.setInterval(() => {
     // moves the rect along instead of adding more
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
     snake.update();
     snake.draw();
+
+    if(snake.eat(fruit)) {
+      fruit.choseInitialPlace();
+    }
+
+    snake.collided();
+    document.querySelector(".score").innerHTML = snake.total;    
+    
   }, 250);
 }());
 
 window.addEventListener('keydown', ((evt) => {
-  console.log(evt);
   const direction = evt.key.replace('Arrow', '');
-  console.log(direction);
   snake.changeDirection(direction);
+
   
   
 }))
--- snake.js
+++ snake.js
@@ -3,13 +3,30 @@ function Snake() {
   this.y = 0;
   this.xspeed = 40;
   this.yspeed = 0;
+  this.total = 0;
+  // this array stores the co-ords of where the snake is
+  this.tail = [];
 
   this.draw = function() {
     ctx.fillStyle = "#FFFFFF";
+
+    // loop through the tatil below and draw each rect
+    for(let i = 0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale)
+
+    }
+
     ctx.fillRect(this.x, this.y, scale, scale)
   }
 
   this.update = function() {
+    // go throught he snake's tail adn shift it to the left
+    for(let i=0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    // this.total is the amount of fruit we've eaten
+    this.tail[this.total - 1] = { x: this.x, y: this.y };
+
     this.x += this.xspeed;
     this.y += this.yspeed;
 
@@ -48,6 +65,25 @@ function Snake() {
         this.xspeed = scale *1;
         this.yspeed = 0;
         break;
+    }
+  }
+
+  this.eat = function(fruit) {
+    if((this.x === fruit.x) && (this.y === fruit.y)) {
+      this.total++;
+      return true;
+    }
+    return false
+  }
+  
+  this.collided = function() {
+    // for every section of the tail, must check if the head is colliding with it
+    for(let i = 0; i < this.tail.length; i++) {
+      if(this.x === this.tail[i].x && this.y === this.tail[i].y) {
+        this.total = 0;
+        this.tail = [];
+        
+      }
     }
   }
 }
